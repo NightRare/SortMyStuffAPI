f3cbe575f3c06f883379add313b0e238482e028c
NightRare <yuan.yuan@live.hk>
2017-11-05 02:36:17 +1300

    Initialise tests project

---
+++ SortMyStuffAPI.Tests/Controllers/RootControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SortMyStuffAPI.Tests.Controllers
+{
+    [TestFixture]
+    class RootControllerTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            
+        }
+
+        [Test]
+        public void EmptyTest()
+        {
+            
+        }
+    }
+}
